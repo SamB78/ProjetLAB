e3d9c2c3468e952edc177034b5b70da2a187b16d
Sam <bertrand.sam78@gmail.com>
2020-05-14 17:50:24 +0200

    tes 1564343

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/user');
+const User = require('../models/user2');
 
 exports.signup = (req, res, next) =>{
     bcrypt.hash(req.body.password, 10)
